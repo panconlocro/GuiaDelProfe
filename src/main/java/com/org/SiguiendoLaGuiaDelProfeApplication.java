4d3c79b5fc7319d7bc80ef05b64176f9103b180e
PANCONLOCRO <rosamariarv10@gmail.com>
2025-05-20 12:09:45 -0500

    estructura inicial del proyecto

---
+++ src/main/java/com/org/SiguiendoLaGuiaDelProfeApplication.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiguiendoLaGuiaDelProfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiguiendoLaGuiaDelProfeApplication.class, args);
+    }
+
+}
